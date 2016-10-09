e6e6ad100933a24b04bb438ef272d6a2bc435422
pat-s <patrick.schratz@gmail.com>
2016-10-09 22:50:23 +0200

    update vignettes/

--- vignettes/sperrorest-vignette.R
+++ vignettes/sperrorest-vignette.R
@@ -1,7 +1,20 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ---- echo=FALSE, cache=FALSE--------------------------------------------
+library(knitr)
+opts_chunk$set(cache = TRUE,
+               fig.align = "center",
+               collapse = TRUE,
+               fig.width = 10,
+               fig.height = 10)
+opts_knit$set(width = 125)
 
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ------------------------------------------------------------------------
+res.rf.sp.par1 <- sperrorest.par(fo, data = d, coords = c("utmx","utmy"), 
+                               model.fun = randomForest,
+                               pred.fun = rf.predfun, 
+                               pred.args = list(fac = "field"),
+                               smp.fun = partition.factor.cv, silent = 2, 
+                               smp.args = list(fac = "field", repetition = 1:4, nfold = 5),
+                               err.pooled = TRUE, err.unpooled = FALSE,
+                               benchmark = TRUE)
+res.rf.sp.par1$benchmark
 
