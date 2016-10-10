109820e4edce63e88d4fa1dab282f935ef8477ae
pat-s <patrick.schratz@gmail.com>
2016-10-10 13:31:40 +0200

    update vignette

--- vignettes/sperrorest-vignette.R
+++ vignettes/sperrorest-vignette.R
@@ -27,3 +27,42 @@ res.lda.sp <- sperrorest(fo, data = maipo, coords = c("utmx","utmy"),
                          err.rep = TRUE, err.fold = FALSE)
 round(summary(res.lda.sp$err.rep), 3)
 
+## ------------------------------------------------------------------------
+res.rf.sp <- sperrorest(fo, data = maipo, coords = c("utmx","utmy"), 
+                        model.fun = randomForest,
+                        pred.fun = rf.predfun, 
+                        pred.args = list(fac = "field"),
+                        smp.fun = partition.factor.cv,
+                        smp.args = list(fac = "field", 
+                                        repetition = 1:4, nfold = 5),
+                        err.rep = TRUE, err.fold = FALSE,
+                        benchmark = TRUE, silent = FALSE)
+res.rf.sp$benchmark
+
+## ------------------------------------------------------------------------
+res.rf.sp.par <- parsperrorest(fo, data = maipo, coords = c("utmx","utmy"), 
+                               model.fun = randomForest,
+                               pred.fun = rf.predfun, 
+                               pred.args = list(fac = "field"),
+                               smp.fun = partition.factor.cv,
+                               smp.args = list(fac = "field", 
+                                               repetition = 1:4, nfold = 5),
+                               par.args = list(par.mode = 1, par.units = 2, 
+                                               lb = T, high = TRUE),
+                               err.rep = TRUE, err.fold = FALSE,
+                               benchmark = TRUE, silent = FALSE)
+res.rf.sp.par$benchmark
+
+## ---- eval = FALSE-------------------------------------------------------
+#  res.rf.sp.par1 <- sperrorest.par(fo, data = maipo, coords = c("utmx","utmy"),
+#                                   model.fun = randomForest,
+#                                   pred.fun = rf.predfun,
+#                                   pred.args = list(fac = "field"),
+#                                   smp.fun = partition.factor.cv,
+#                                   smp.args = list(fac = "field",
+#                                                   repetition = 1:4, nfold = 5),
+#                                   par.units = 2, silent = 2,
+#                                   err.fold = T, err.rep = T,
+#                                   benchmark = TRUE)
+#  res.rf.sp.par1$benchmark
+
