efc2109642cbc29ce5e8a10d01a3bdcf723cf83f
pat-s <patrick.schratz@gmail.com>
2016-12-09 18:00:11 +0100

    update Bibliography in vignette

--- vignettes/sperrorest-vignette.R
+++
@@ -1,9 +0,0 @@
-## ---- echo=FALSE, cache=FALSE--------------------------------------------
-library(knitr)
-opts_chunk$set(cache = TRUE,
-               fig.align = "center",
-               collapse = TRUE,
-               fig.width = 7,
-               fig.height = 5)
-opts_knit$set(width = 125)
-
