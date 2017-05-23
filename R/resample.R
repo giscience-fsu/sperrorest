f73de308357658f132fd66cdf3f5523365d3bde4
pat-s <patrick.schratz@gmail.com>
2017-05-23 20:56:17 +0200

    tidy

--- R/resample.R
+++ R/resample.R
@@ -32,42 +32,46 @@ resample.strat.uniform <- function(data, param = list(strat = "class", nstrat = 
                                                       replace = FALSE))
 {
   # Old version:
-  if (!is.null(param$response))
-  {
+  if (!is.null(param$response)) {
     warning("'param$response' argument in 'resample.strat.uniform' renamed to 'strat';\n modify your code accordingly") # nocov
     if (is.null(param$strat)) # nocov
       param$strat <- param$response # nocov
   } 
   
   # Use defaults if not specified:
-  if (is.null(param$strat)) 
+  if (is.null(param$strat)) {
     param$strat <- "class"
-  if (is.null(param$nstrat)) 
+  }
+  if (is.null(param$nstrat)) {
     param$nstrat <- Inf
-  if (is.null(param$replace)) 
+  }
+  if (is.null(param$replace)) {
     param$replace <- FALSE
+  }
   
   stopifnot((length(param$strat) == 1) | (length(param$strat) == nrow(data)))
-  if (length(param$strat == 1))
-  {
+  if (length(param$strat == 1)) {
     strat <- data[, param$strat]
-  } else strat <- param$strat
-  if (!is.factor(strat)) 
+  } else {
+    strat <- param$strat
+  }
+  if (!is.factor(strat)) {
     stop("'strat' must either be a vector of factor type, or the name of 
          a factor variable in 'data'")
+  }
   # Each factor level must have at least one sample, otherwise sampling within this
   # level is impossible:
   minstrat <- min(tapply(strat, strat, length))
   stopifnot(minstrat >= 1)
   # might want to change this to a warning.???
   
-  if (!param$replace) 
+  if (!param$replace) {
     param$nstrat <- min(param$nstrat, minstrat)
+  }
   
   # Uniform sampling within each stratum:
   sel <- c()
-  for (lev in levels(strat))
-  {
+  for (lev in levels(strat)) {
     wh <- sample(which(strat == lev), size = param$nstrat, replace = param$replace)
     sel <- c(sel, wh)
   }
@@ -96,21 +100,23 @@ resample.strat.uniform <- function(data, param = list(strat = "class", nstrat = 
 #' 
 #' @examples
 #' data(ecuador) # Muenchow et al. (2012), see ?ecuador
-#' d = resample.uniform(ecuador, param = list(strat = 'slides', n = 200))
+#' d <- resample.uniform(ecuador, param = list(strat = 'slides', n = 200))
 #' nrow(d) # == 200
 #' sum(d$slides == 'TRUE')
 #' 
 #' @export
-resample.uniform <- function(data, param = list(n = Inf, replace = FALSE))
-{
+resample.uniform <- function(data, param = list(n = Inf, replace = FALSE)) {
   # Apply defaults if missing from parameter list:
-  if (is.null(param$n)) 
+  if (is.null(param$n)) {
     param$n <- Inf
-  if (is.null(param$replace)) 
+  }
+  if (is.null(param$replace)) {
     param$replace <- FALSE
+  }
   
-  if (!param$replace) 
+  if (!param$replace) {
     param$n <- min(param$n, nrow(data))
+  }
   
   # Uniform sampling with or without replacement:
   sel <- sample(nrow(data), size = param$n, replace = param$replace)
@@ -146,25 +152,31 @@ resample.uniform <- function(data, param = list(n = Inf, replace = FALSE))
 #' sum(d$slides == 'TRUE')
 #' 
 #' @export
-resample.factor <- function(data, param = list(fac = "class", n = Inf, replace = FALSE))
-{
-  if (is.null(param$fac)) 
+resample.factor <- function(data, param = list(fac = "class", n = Inf, 
+                                               replace = FALSE)) {
+  if (is.null(param$fac)) {
     param$fac <- "class"
-  if (is.null(param$replace)) 
+  }
+  if (is.null(param$replace)) {
     param$replace <- FALSE
+  }
   stopifnot((length(param$fac) == 1) || (length(param$fac) == nrow(data)))
-  if (length(param$fac == 1))
-  {
+  if (length(param$fac == 1)) {
     fac <- data[, param$fac]
-  } else fac <- param$fac
-  if (!is.factor(fac)) 
+  } else {
+    fac <- param$fac
+  }
+  if (!is.factor(fac)) {
     stop("'fac' must either be a vector of factor type, or the name of a 
          factor variable in 'data'")
+  }
   fac <- factor(fac)
-  if (is.null(param$n) || is.infinite(param$n)) 
+  if (is.null(param$n) || is.infinite(param$n)) {
     param$n <- nlevels(fac)
-  if (!param$replace) 
+  }
+  if (!param$replace) {
     param$n <- min(param$n, nrow(data))
+  }
   sel <- sample(levels(fac), size = param$n, replace = param$replace)
   sel <- fac %in% sel
   return(data[sel, ])
