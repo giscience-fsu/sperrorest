c399512e86304c1143238f0a8df76d6bc5dfbeea
pat-s <patrick.schratz@gmail.com>
2017-06-01 22:29:57 +0200

    add function removing_missing_levels

--- R/helper_funs.R
+++ R/helper_funs.R
@@ -110,6 +110,10 @@ runfolds <- function(j = NULL, current_sample = NULL, data = NULL, i = NULL,
   if (importance) {
     nd_bak <- nd
   }
+
+  # account for possible missing factor levels in test data
+  nd <- remove_missing_levels(fit, nd)
+
   # Apply model to test sample:
   pargs <- c(list(object = fit, newdata = nd), pred_args)
   if (is.null(pred_fun)) {
@@ -375,3 +379,64 @@ transfer_parallel_output <- function(my_res = NULL, res = NULL, impo = NULL,
   return(list(pooled_error = pooled_error, impo = impo,
               res = res))
 }
+
+#' @title remove_missing_levels
+#' @description Accounts for missing factor levels present only in test data
+#' but not in train data by setting values to NA
+#'
+#' @import magrittr
+#'
+#' @param fit fitted model on training data
+#'
+#' @param test_data data to make predictions for
+#'
+#' @return data.frame with matching factor levels to fitted model
+#'
+#' @keywords internal
+#'
+#' @export
+remove_missing_levels <- function(fit, test_data) {
+
+  # https://stackoverflow.com/a/39495480/4185785
+
+  # drop empty factor levels in test data
+  test_data %>%
+    droplevels() %>%
+    as.data.frame()-> test_data
+
+  # Obtain factor predictors in the model and their levels
+
+  factors <- (gsub("[-^0-9]|as.factor|\\(|\\)", "",
+                   names(unlist(fit$xlevels))))
+  factor_levels <- unname(unlist(fit$xlevels))
+  model_factors <- as.data.frame(cbind(factors, factor_levels))
+
+  # Select column names in test data that are factor predictors in
+  # trained model
+
+  predictors <- names(test_data[names(test_data) %in% factors])
+
+  # For each factor predictor in your data, if the level is not in the model,
+  # set the value to NA
+
+  for (i in 1:length(predictors)) {
+    found <- test_data[, predictors[i]] %in% model_factors[
+      model_factors$factors == predictors[i], ]$factor_levels
+    if (any(!found)) {
+      # track which variable
+      var <- predictors[i]
+      # set to NA
+      test_data[!found, predictors[i]] <- NA
+      # drop empty factor levels in test data
+      test_data %>%
+        droplevels() -> test_data
+      # issue warning to console
+      message(sprintf(paste0("Setting missing levels in '%s', only present",
+                             " in test data but missing in train data,",
+                             " to 'NA'."),
+                      var))
+    }
+  }
+  return(test_data)
+}
+
