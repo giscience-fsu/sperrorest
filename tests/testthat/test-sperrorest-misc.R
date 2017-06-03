3f85ff70c99cf3ab313f4790b4eb866c2b61e576
pat-s <patrick.schratz@gmail.com>
2017-06-03 15:01:44 +0200

    add add.distance test

--- tests/testthat/test-sperrorest-misc.R
+++ tests/testthat/test-sperrorest-misc.R
@@ -9,3 +9,14 @@ test_that("dataset_distance works correctly", {
   out <- dataset_distance(df, df) # == 0
   expect_equal(out, 0)
 })
+
+
+# add.distance Sat Jun  3 14:45:34 2017 ------------------------------
+
+test_that("add.distance works correctly", {
+  data(ecuador) # Muenchow et al. (2012), see ?ecuador
+  nsp_parti <- partition_cv(ecuador)
+  nsp_parti <- add.distance(nsp_parti, ecuador)
+
+  expect_length(nsp_parti[[1]], 10)
+})
