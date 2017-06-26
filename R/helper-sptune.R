de16a4b19050c9e06c47714e56f874071219a3d9
pat-s <patrick.schratz@gmail.com>
2017-06-27 00:10:42 +0200

    - add maxent_cv_err - add sptune_maxent

--- R/helper-sptune.R
+++ R/helper-sptune.R
@@ -277,6 +277,164 @@ rf_cv_err <- function(ntree = NULL, mtry = NULL, train = NULL, test = NULL,
   return(perf_measures)
 }
 
+#' @title maxent_cv_err
+#' @description Calculates AUROC for different beta_multiplier and
+#' feature_classes values
+#'
+#' @importFrom ROCR prediction performance
+#' @importFrom dismo maxent
+#' @importFrom dplyr semi_join
+#'
+#' @param formula model formula
+#'
+#' @inheritParams beta_multiplier
+#'
+#' @inheritParams feature_classes
+#'
+#' @param train training data
+#'
+#' @param test testing data
+#'
+#' @param response response variable
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \dontrun{
+#' data(maxent_pred)
+#' data(maxent_response)
+#'
+#' parti <- partition_kmeans(basque, nfold = 5)
+#' train <- maxent_pred[parti[[1]][[1]]$train, ]
+#' test <- maxent_pred[parti[[1]][[1]]$test, ]
+#'
+#' out <- maxent_cv_err(beta_multiplier = 1,
+#'                      feature_classes = "LQ",
+#'                      train = train, test = test, x = maxent_pred,
+#'                      p = maxent_response, response = "diplo01",
+#'                      absence = TRUE)
+#' @export
+maxent_cv_err <- function(beta_multiplier = NULL, feature_classes = NULL,
+                          train = NULL, test = NULL, x = NULL, p = NULL,
+                          response = NULL, absence = FALSE, ...) {
+
+  # account for feature classes
+  if (feature_classes == "L") {
+    linear <- TRUE
+    quadratic <- FALSE
+    product <- FALSE
+    threshold <- FALSE
+    hinge <- FALSE
+  } else if (feature_classes == "Q") {
+    linear <- FALSE
+    quadratic <- TRUE
+    product <- FALSE
+    threshold <- FALSE
+    hinge <- FALSE
+  } else if (feature_classes == "H") {
+    linear <- FALSE
+    quadratic <- FALSE
+    product <- FALSE
+    threshold <- FALSE
+    hinge <- TRUE
+  } else if (feature_classes == "T") {
+    linear <- FALSE
+    quadratic <- FALSE
+    product <- FALSE
+    threshold <- TRUE
+    hinge <- FALSE
+  } else if (feature_classes == "LQ") {
+    linear <- TRUE
+    quadratic <- TRUE
+    product <- FALSE
+    threshold <- FALSE
+    hinge <- FALSE
+  } else if (feature_classes == "HQ") {
+    linear <- FALSE
+    quadratic <- TRUE
+    product <- FALSE
+    threshold <- FALSE
+    hinge <- TRUE
+  } else if (feature_classes == "LQP") {
+    linear <- TRUE
+    quadratic <- TRUE
+    product <- TRUE
+    threshold <- FALSE
+    hinge <- FALSE
+  } else if (feature_classes == "LQT") {
+    linear <- TRUE
+    quadratic <- TRUE
+    product <- FALSE
+    threshold <- TRUE
+    hinge <- FALSE
+  } else if (feature_classes == "QHP") {
+    linear <- FALSE
+    quadratic <- TRUE
+    product <- TRUE
+    threshold <- FALSE
+    hinge <- TRUE
+  } else if (feature_classes == "QHT") {
+    linear <- FALSE
+    quadratic <- TRUE
+    product <- FALSE
+    threshold <- TRUE
+    hinge <- TRUE
+  } else if (feature_classes == "QHT") {
+    linear <- FALSE
+    quadratic <- TRUE
+    product <- TRUE
+    threshold <- TRUE
+    hinge <- TRUE
+  }
+
+  # subset 'p' to respective subsets
+  x$pa <- p
+  train_tmp <- suppressMessages(semi_join(x, train))
+  p_train <- train_tmp$pa
+  test_tmp <- suppressMessages(semi_join(x, test))
+  p_test <- test_tmp$pa
+
+  # # predict args here??
+  # sprintf(paste0("outputformat = raw,betamultiplier = %s,",
+  #              "linear = %s,quadratic = %s,product = %s,threshold = %s,",
+  #              "hinge = %s"), beta_multiplier, linear, quadratic, product,
+  #              threshold, hinge) -> my_args
+  #   str_split(my_args, pattern = ",")[[1]] -> my_args
+
+  # predict args here??
+  sprintf(paste0("betamultiplier=%s,",
+               "linear=%s,quadratic=%s,product=%s,threshold=%s,",
+               "hinge=%s"), beta_multiplier, linear, quadratic, product,
+               threshold, hinge) -> my_args
+    str_split(my_args, pattern = ",")[[1]] -> my_args
+
+    # we need to subset 'p' here to match with 'train' data
+
+
+    args <- list(x = train, p = p_train, args = my_args)
+
+  # fit model
+  fit <- do.call(maxent, args)
+
+  # p must be numeric here
+  prev_pa <- mean(as.numeric(as.character(p)))
+
+  # post-process predict results http://onlinelibrary.wiley.com/store/10.1111/2041-210X.12252/asset/supinfo/mee312252-sup-0003-Appendix5.R?v=1&s=13755a940831aa9186cf931209e826a304e9868e
+  if (absence == TRUE) {
+    pred <- dismo::predict(fit, x = test, args = "outputformat=raw")
+    numbg <- fit@results["X.Background.points", ] #number of points in background
+    pred <- pred * prev_pa * numbg #adjusted raw output (probabilities)
+    pred[pred > 1] <- 1   #clipped to 1
+  } else {
+    pred <- dismo::predict(fit, x = test, args = "outputformat=logistic")
+  }
+
+  # calculate error measures
+  perf_measures <- err_default(p_test, pred)
+
+  return(perf_measures)
+}
+
 #' @title check_response_type
 #' @description Checks response type of input and sets error measure
 #'
