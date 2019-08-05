92b65cba16ba1ec79b4ba872537c2d5cde0fcd09
pat-s <patrick.schratz@gmail.com>
2019-08-05 09:09:48 +0200

    fix 'length > 1 in coercion to logical'

--- R/sperrorest.R
+++ R/sperrorest.R
@@ -541,7 +541,7 @@ sperrorest <- function(formula, data, coords = c("x", "y"),
                     test_param = test_param,
                     pooled_obs_test = pooled_obs_test, err_fun = err_fun)))
           # check if run was sufficient
-          if (length(my_res) > 1 && my_res == "NULL") {
+          if (length(my_res) > 1 && all(my_res == "NULL")) {
             stop(paste0("No output was received from sperrorest.\n", # nocov start # nolint
                         "If you are on macOS either run R in 'Vanilla' mode or",
                         " use another parallel mode.")) # nocov end
